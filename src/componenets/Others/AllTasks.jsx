5ae9cafaa6c938042ccc50ea006dc8d422492812
arfath-ahmed <155220118+arfath-ahmed@users.noreply.github.com>
2025-01-23 21:07:43 +0530

    Update AllTasks.jsx

--- src/componenets/Others/AllTasks.jsx
+++ src/componenets/Others/AllTasks.jsx
@@ -5,25 +5,25 @@ const AllTasks = () => {
     const [userData, setUserData] = useContext(AuthContext);
 
     return (
-        <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-60'>
-            <div id="emp" className='bg-emerald-700 mb-2 py-2 px-4 flex justify-between rounded'>
-                <h2 className='w-1/6 text-lg font-medium flex justify-center max-[450px]: w-1/12 text-[8px] font-bold'>Employee Name</h2>
-                <h3 className='w-1/6 text-lg font-medium flex justify-center max-[450px]: w-1/12 text-[8px] font-bold'>Employee ID</h3>
-                <h3 className='w-1/6 text-lg font-medium flex justify-center max-[450px]: w-1/12 text-[8px] font-bold'>New Tasks</h3>
-                <h5 className='w-1/6 text-lg font-medium flex justify-center max-[450px]: w-1/12 text-[8px] font-bold'>Active Tasks</h5>
-                <h5 className='w-1/6 text-lg font-medium flex justify-center max-[450px]: w-1/12 text-[8px] font-bold'>Completed</h5>
-                <h5 className='w-1/6 text-lg font-medium flex justify-center max-[450px]: w-1/12 text-[8px] font-bold'>Failed</h5>
+        <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-60 overflox-x-auto'>
+            <div id="emp" className='bg-emerald-700 mb-2 py-2 px-0 flex justify-evenly rounded'>
+                <h2 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>Employee Name</h2>
+                <h3 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>Employee ID</h3>
+                <h3 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>New Tasks</h3>
+                <h5 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>Active Tasks</h5>
+                <h5 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>Completed</h5>
+                <h5 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>Failed</h5>
             </div>
             <div className='h-[80%] overflow-auto'>
                 {userData.map(function (elem, idx) {
                     return (
                         <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
-                            <h2 className='w-1/6 text-lg font-medium flex justify-center'>{elem.firstName}</h2>
-                            <h3 className='w-1/6 text-lg font-medium flex justify-center'>{elem.id}</h3>
-                            <h3 className='w-1/6 text-lg font-medium flex justify-center text-blue-600'>{elem.taskNumber.newTask}</h3>
-                            <h5 className='w-1/6 text-lg font-medium flex justify-center text-yellow-400'>{elem.taskNumber.active}</h5>
-                            <h5 className='w-1/6 text-lg font-medium flex justify-center text-white'>{elem.taskNumber.completed}</h5>
-                            <h5 className='w-1/6 text-lg font-medium flex justify-center text-red-600'>{elem.taskNumber.failed}</h5>
+                            <h2 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>{elem.firstName}</h2>
+                            <h3 className='w-1/12 text-[8px] font-medium flex justify-center sm:text-xs leading-none'>{elem.id}</h3>
+                            <h3 className='w-1/12 text-[8px] font-medium flex justify-center text-blue-600 sm:text-xs leading-none'>{elem.taskNumber.newTask}</h3>
+                            <h5 className='w-1/12 text-[8px] font-medium flex justify-center text-yellow-400 sm:text-xs leading-none'>{elem.taskNumber.active}</h5>
+                            <h5 className='w-1/12 text-[8px] font-medium flex justify-center text-white sm:text-xs leading-none'>{elem.taskNumber.completed}</h5>
+                            <h5 className='w-1/12 text-[8px] font-medium flex justify-center text-red-600 sm:text-xs leading-none'>{elem.taskNumber.failed}</h5>
                         </div>)
                 })}
             </div>
